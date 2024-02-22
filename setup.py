9e244b1f4309722b1e69955712aa09e420e8cc9b
Christian Bönning <hexa2k9@users.noreply.github.com>
2024-02-22 15:36:27 +0100

    Switch to libdyson-neon

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     classifiers=[],
     python_requires='>=3',
     install_requires=[
-        'libdyson==0.8.9',
+        'libdyson-neon==1.3.0',
         'prometheus-client==0.13.1',
         'python_json_logger==2.0.2'
     ],
