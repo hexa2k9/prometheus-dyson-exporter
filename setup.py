ca0ca20ca559a3beebfc793cd263fadb2ea0a184
Christian Bönning <christian.boenning@metro-markets.de>
2024-11-06 16:07:25 +0100

    Build Docker image through Actions, Configure Dependabot

--- setup.py
+++ setup.py
@@ -3,6 +3,16 @@ from setuptools import setup
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+def parse_requirements(filename):
+    """Load requirements from a pip requirements file"""
+    with open(filename, "r") as fd:  # pylint: disable=unspecified-encoding
+        lines = []
+        for line in fd:
+            line = line.strip()
+            if line and not line.startswith("#"):
+                lines.append(line)
+    return lines
+
 setup(
     name='prometheus-dyson-exporter',
     packages=['dyson_exporter'],
@@ -17,12 +27,7 @@ setup(
     keywords=['prometheus', 'dyson'],
     classifiers=[],
     python_requires='>=3',
-    install_requires=[
-        'paho-mqtt==1.6.1',
-        'libdyson-neon==1.4.1',
-        'prometheus-client==0.21.0',
-        'python_json_logger==2.0.7'
-    ],
+    install_requires=parse_requirements("requirements.txt"),
     entry_points={
         'console_scripts': [
             'dyson-exporter=dyson_exporter.exporter:main',
